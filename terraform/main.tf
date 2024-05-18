35cb314f7326566dd37624cb71f1e21381c9695c
suresh-akidev <48638555+suresh-akidev@users.noreply.github.com>
2024-05-18 11:03:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
   }
 }
 
+terraform {
+ backend "gcs" {
+   bucket  = "dude-abcd-xyz"
+   prefix  = "terraform/state"
+ }
+}
+
 resource "google_storage_bucket" "auto-expire" {
   project       = "sodium-atrium-308005"
   name          = "no-public-access-bucket-xyz"
